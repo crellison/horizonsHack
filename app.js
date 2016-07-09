bba14ea0794a318ce2881ab1696acb5c3f5f65ae
Ying Hang Eng <eyinghang@gmail.com>
2016-07-09 18:13:28 -0400

    massive commit

--- app.js
+++ app.js
@@ -13,75 +13,42 @@ var mongoose = require('mongoose');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 // ----------------------------------------------
 // Import models
 // ----------------------------------------------
 var User = require('./models/models').User;
 
-// Passport stuff here
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
-app.use(session({
-    secret: process.env.SECRET,
-    name: 'Hmweradeh',
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    proxy: true,
-    resave: true,
-    saveUninitialized: true
-}));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser(function(user, done) {
-  done(null, user._id);
+// configure our app to handle CORS requests
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  next();
 });
 
-passport.deserializeUser(function(id, done) {
-  models.User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
+// log all requests to the console 
+app.use(morgan('dev'));
 
-// passport strategy
-passport.use(new LocalStrategy(function(username, password, done) {
-    // Find the user with the given username
-    User.findOne({ email: username }, function (err, user) {
-      // if there's an error, finish trying to authenticate (auth failed)
-      if (err) {
-        console.error(err);
-        return done(err);
-      }
-      // if no user present, auth failed
-      if (!user) {
-        console.log(user);
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      // if passwords do not match, auth failed
-      var validPassword = user.comparePassword(password);
-      if (!validPassword) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      // auth has has succeeded
-      return done(null, user);
-    });
-  }
-));
+// ROUTES FOR OUR API =================
+// ====================================
 
-app.use('/', auth(passport));
-app.use('/', routes);
+// API ROUTES ------------------------
+var apiRoutes = require('./routes/api')(app, express);
+app.use('/api', apiRoutes);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
