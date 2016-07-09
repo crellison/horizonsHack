c94528e8b9b92ff07b25680717b898edd8618316
Ying Hang Eng <eyinghang@gmail.com>
2016-07-09 19:24:28 -0400

    restful api login works forreal

--- routes/api.js
+++ routes/api.js
@@ -73,33 +73,27 @@ module.exports = function(app, express){
 
 	    // verifies secret and checks exp
 	    jwt.verify(token, superSecret, function(err, decoded) {      
-	      if (err)
-	        return res.json({ success: false, message: 'Failed to authenticate token.' });    
-	      else
-	        // if everything is good, save to request for use in other routes
-	        req.decoded = decoded;    
-	    });
-
-	  } else {
-
-	    // if there is no token
-	    // return an HTTP response of 403 (access forbidden) and an error message
-
-	    // Fucking stupid ass workaround
-	    jwt.verify(token, superSecret, function(err, decoded) {      
 	      if (err){
 	      	res.status(403).send({ 
 	        	success: false, 
 	        	message: 'Failed to authenticate token.' 
-	    	}); 
-	      }     
-	      else
+	    	});
+	      }
+	      else{
 	        // if everything is good, save to request for use in other routes
-	        req.decoded = decoded;    
+	        req.decoded = decoded;   
+	        next(); // make sure we go to the next routes and don't stop here 
+	      }
 	    });
-	  }
 
-	  next(); // make sure we go to the next routes and don't stop here
+	  } else {
+	  	// if there is no token
+	    // return an HTTP response of 403 (access forbidden) and an error message
+   	 	res.status(403).send({ 
+   	 		success: false, 
+   	 		message: 'No token provided.' 
+   	 	});
+	  }
 	});
 
 	// accessed at GET http://localhost:3000/api
