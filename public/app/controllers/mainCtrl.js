48466b474c94f4eb25b50d7e4d6fd59d2bd9335e
Will Yoo <wyoo1997@gmail.com>
2016-07-10 08:01:46 -0400

    playing with animations

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -1,81 +1,79 @@
 angular.module('mainCtrl', ["chart.js"])
 
-.controller('mainController', function($rootScope, $scope,  $location, Auth) {
+.controller('mainController', function($rootScope, $scope, $location, Auth) {
 
 
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
-  $scope.onClick = function (points, evt) {
-    console.log(points, evt);
-  };
-  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
-  $scope.options = {
-    scales: {
-      yAxes: [
-        {
-          id: 'y-axis-1',
-          type: 'linear',
-          display: true,
-          position: 'left'
-        },
-        {
-          id: 'y-axis-2',
-          type: 'linear',
-          display: true,
-          position: 'right'
+    //Chart.js Stuff
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+    ];
+    $scope.onClick = function(points, evt) {
+        console.log(points, evt);
+    };
+    $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+    $scope.options = {
+        scales: {
+            yAxes: [{
+                id: 'y-axis-1',
+                type: 'linear',
+                display: true,
+                position: 'left'
+            }, {
+                id: 'y-axis-2',
+                type: 'linear',
+                display: true,
+                position: 'right'
+            }]
         }
-      ]
-    }
-  };
+    };
 
 
-	var vm = this;
+    var vm = this;
 
-	// get info if a person is logged in
-	vm.loggedIn = Auth.isLoggedIn();
+    // get info if a person is logged in
+    vm.loggedIn = Auth.isLoggedIn();
 
-	// check to see if a user is logged in on every request
-	$rootScope.$on('$routeChangeStart', function() {
-		vm.loggedIn = Auth.isLoggedIn();	
+    // check to see if a user is logged in on every request
+    $rootScope.$on('$routeChangeStart', function() {
+        vm.loggedIn = Auth.isLoggedIn();
 
-		// get user information on page load
-		Auth.getUser()
-			.then(function(data) {
-				console.log(data);
-				vm.user = data.data;
-			});	
-	});	
+        // get user information on page load
+        Auth.getUser()
+            .then(function(data) {
+                console.log(data);
+                vm.user = data.data;
+            });
+    });
 
-	// function to handle login form
-	vm.doLogin = function() {
-		vm.processing = true;
+    // function to handle login form
+    vm.doLogin = function() {
+        vm.processing = true;
 
-		// clear the error
-		vm.error = '';
+        // clear the error
+        vm.error = '';
 
-		Auth.login(vm.loginData.email, vm.loginData.password)
-			.success(function(data) {
-				vm.processing = false;			
+        Auth.login(vm.loginData.email, vm.loginData.password)
+            .success(function(data) {
+                vm.processing = false;
 
-				// if a user successfully logs in, redirect to users page
-				if (data.success)			
-					$location.path('/');
-				else 
-					vm.error = data.message;
-				
-			});
-	};
+                // if a user successfully logs in, redirect to users page
+                if (data.success)
+                    $location.path('/');
+                else
+                    vm.error = data.message;
 
-	// function to handle logging out
-	vm.doLogout = function() {
-		Auth.logout();
-		vm.user = '';
-		
-		$location.path('/login');
-	};
+            });
+    };
+
+    // function to handle logging out
+    vm.doLogout = function() {
+        Auth.logout();
+        vm.user = '';
+
+        $location.path('/login');
+    };
 
 });
